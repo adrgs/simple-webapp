8e96be48293bfe58fcce4aed88dfb214aef17679
adrgs <albastroiudragos@gmail.com>
2024-03-14 13:00:42 +0100

    Add javascript

--- javascript/app.js
+++ javascript/app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const path = require("path");
+const session = require("express-session");
+const fs = require("fs");
 
 const app = express();
 const port = 7777;
@@ -8,8 +10,58 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "html");
 app.engine("html", require("ejs").renderFile);
 
+app.use(
+  session({
+    secret: "your_secret_key", // Different on remote :)
+    resave: false, // Don't save unchanged sessions
+    saveUninitialized: false, // Don't create session for uninitialized requests
+    cookie: {
+      maxAge: 60 * 60 * 1000, // Cookie expires in 1 hour
+    },
+  }),
+);
+
 app.get("/", (req, res) => {
-  res.render("index.html");
+  if (!req.session.username) {
+    req.session.username = "Guest";
+    req.session.permissions = {};
+  }
+
+  res.render("index.html", { username: req.session.username });
+});
+
+function mergeObjects(dest, src) {
+  for (let key in src) {
+    if (
+      (typeof dest[key] === "object" || typeof dest[key] === "function") &&
+      typeof src[key] === "object"
+    ) {
+      mergeObjects(dest[key], src[key]);
+    } else {
+      dest[key] = src[key];
+    }
+  }
+}
+
+app.get("/echo", (req, res) => {
+  console.log(req.query);
+
+  let obj = {};
+  mergeObjects(obj, req.query);
+
+  res.render("index.html", { username: obj.echo ?? "Nothing to echo" });
+});
+
+app.get("/admin", (req, res) => {
+  console.log(req.session.permissions);
+  if (!req.session.permissions || !req.session.permissions.admin) {
+    res.status(403).send("You do not have permission to access this page");
+    return;
+  }
+
+  const flag = fs.readFileSync("../flag.txt", "utf-8");
+
+  res.render("index.html", { username: flag });
 });
 
 app.listen(port, () => {
